a8f3d2e6406c8ad614feabd942ad51b95abf6797
David Menahem <dvmena39@gmail.com>
2024-01-12 19:35:25 +0200

    coupon images update

--- src/Components/Projects/Coupon/Coupon.tsx
+++ src/Components/Projects/Coupon/Coupon.tsx
@@ -1,8 +1,12 @@
 import { useEffect } from "react";
 import "./Coupon.css"
 import couponAdd from "../../../assets/coupon-add-big.png";
-import couponComapnies from "../../../assets/coupon-companies-big.png";
-import couponLogin from "../../../assets/coupon-login-big.png";
+import couponLogin from "../../../assets/coupon-login.png";
+import companies from "../../../assets/companies.png";
+import companiesCoupons from "../../../assets/company-coupons.png";
+import companyCouponElectricity from "../../../assets/company-coupon-electricity.png";
+import customerCoupons from "../../../assets/customer-coupons.png";
+import customers from "../../../assets/customers.png";
 
 function Coupon(): JSX.Element{
     useEffect(() =>{
@@ -25,11 +29,31 @@ function Coupon(): JSX.Element{
                     </figure>
                     <figure className="figure">
                         <figcaption className="caption">List of companies</figcaption>
-                        <img src={couponComapnies} alt="Login coupon" title="Login" width={1024} height={768}></img>
+                        <img src={companies} alt="List of companies" title="Login" width={1024} height={768}></img>
+                    </figure>
+                    <figure className="figure">
+                        <figcaption className="caption">List of customers</figcaption>
+                        <img src={customers} alt="List of customers" title="Login" width={1024} height={768}></img>
+                    </figure>
+                    <figure className="figure">
+                        <figcaption className="caption">List of coupons by customer</figcaption>
+                        <img src={customerCoupons} alt="List of coupons by customer" title="Login" width={1024} height={768}></img>
+                    </figure>
+                    <figure className="figure">
+                        <figcaption className="caption">List of coupons by company</figcaption>
+                        <img src={companiesCoupons} alt="List of coupons by company" title="Login" width={1024} height={768}></img>
+                    </figure>
+                    <figure className="figure">
+                        <figcaption className="caption">List of coupons by company in electricity category</figcaption>
+                        <img src={companyCouponElectricity} alt="List of coupons by company in electricity category" title="Login" width={1024} height={768}></img>
                     </figure>
                     <figure className="figure">
                         <figcaption className="caption">Login to coupon system</figcaption>
-                        <img src={couponLogin} alt="List of companaies" width={1024} height={768} title="Companies list"></img>
+                        <img src={couponLogin} alt="Login to coupon system" width={1024} height={768} title="Companies list"></img>
+                    </figure>
+                    <figure className="figure">
+                        <figcaption className="caption">Add a coupon</figcaption>
+                        <img src={couponAdd} alt="Add coupon" title="Add a coupon" width="1024" height="768"></img>
                     </figure>
             </div>
         </div>);
